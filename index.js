91df4b248d18cdc64a6454ad5ac200838fd195a8
Sai Ramya Linga <sairamyalinga@gmail.com>
2023-11-09 17:53:08 -0700

    Adding HTML Form action

--- index.js
+++ index.js
@@ -1,9 +1,19 @@
 
 var express = require('express');
 var app = express();
+var bodyParser = require('body-parser'); 
+app.use(bodyParser.urlencoded({ extended: true })); 
 
-app.get('/hello',function(req,res){
-    res.sendFile('index.html',{root:'/Users/saira/Documents/Web/sample_aws_project'})
+app.get('/', function(req,res){
+  res.sendFile('index.html',{root:'/Users/saira/Documents/Web/sample_aws_project'})
+})
+
+app.post("/", function(req,res){
+  var Name =  req.body.Fullname;
+  var Uni = req.body.University;
+  res.json({FullName: Name, University: Uni})
+})
+app.get('/hello',function(req,res){    
     res.json("Hello")
 })
 
